928e3e041d28d02c676487365da8c647a232daea
Sijia Chen <CSJDeveloper@users.noreply.github.com>
2023-09-20 21:43:30 -0400

    Combined multiple datasets running code files into one

--- examples/ChainOfThought/ChainOfThought.py
+++ examples/ChainOfThought/ChainOfThought.py
@@ -5,24 +5,100 @@ The implementation of Chain Of Thought [1].
 """
 
 import os
+import random
 import resource
 
-from vgbase.utils.envs_utils import define_env
-from vgbase.models.LMs import chatgpts
 
-# from vgbase.datasets.mmlu import DataSource as mmlu_datasource
-from vgbase.datasets.mmlu import (
+from vgbase.utils.envs_utils import define_env
+from vgbase.config import Config
+from dotenv import load_dotenv
+
+from llmpebase.models.LMs import chatgpts
+
+# from llmpebase.datasets.mmlu import DataSource as mmlu_datasource
+from llmpebase.datasets.mmlu import (
     DataSource as mmlu_datasource,
 )
-from vgbase.models.LMs_prompting import mmlu_prompt
-
-from vgbase.config import Config
-
-from dotenv import load_dotenv
+from llmpebase.datasets.gsm8k import (
+    DataSource as gsm8k_datasource,
+)
+from llmpebase.models.LMs_prompting import mmlu_prompt
+from llmpebase.models.LMs_prompting import gsm8k_prompt
 
 # there must have a .env file containing keywords
 # OPENAI_ORGAN_KEY and OPENAI_API_KEY
 load_dotenv()
+
+
+def do_model_request(chatgpt_api, request_prompt):
+    """Do model request."""
+    ipt_msg = chatgpt_api.create_format_input(
+        user_prompt=request_prompt,
+        sys_prompt="Follow the given examples and answer the question.",
+    )
+    model_responses = chatgpt_api.perform_request(
+        request_input=ipt_msg, per_request_responses=3
+    )
+    print("model_responses: ", model_responses)
+
+    extract_answer = chatgpt_api.extract_answers(model_responses)
+    print(extract_answer)
+    extract_target_answer = chatgpt_api.extract_response_target_answer(extract_answer)
+    print(extract_target_answer)
+
+
+def eval_mmlu(chatgpt_api, eval_config):
+    """Eval the MMLU."""
+    mmlu_data = mmlu_datasource()
+    train_set = mmlu_data.get_train_set()
+    test_set = mmlu_data.get_test_set()
+
+    input_prompter = mmlu_prompt.MMLUStandardPrompt()
+    # input_prompt = mmlu_prompt.MMLUCoTPrompt(
+    #     cot_filepath="examples/LMs/ChainOfThought/mmlu-cot-claude.json"
+    # )
+    chatgpt_api.set_target_answer_format(input_prompter.answer_format_str)
+
+    n_shots = eval_config["n_shots"]
+
+    print("n_shots: ", n_shots)
+    assert n_shots <= 5
+    for task_name in train_set.tasks_name:
+        train_samples = train_set[task_name, -1]
+        test_task_samples = test_set[task_name, -1]
+        shots = train_samples[:n_shots]
+        for test_sample in test_task_samples:
+            request_prompt = input_prompter.organize_test_prompt(
+                task_name, shots, test_sample
+            )
+            do_model_request(chatgpt_api, request_prompt)
+
+
+def eval_gsm8k(chatgpt_api, eval_config):
+    """Eval the GSM8K."""
+    gsm_data = gsm8k_datasource()
+    train_set = gsm_data.get_train_set()
+    test_set = gsm_data.get_test_set()
+
+    # input_prompt = gsm8k_prompt.GSM8KStandardPrompt()
+    input_prompter = gsm8k_prompt.GSM8KCoTPrompt(
+        cot_filepath="examples/ChainOfThought/gsm8k_prompt/prompt_6_9step.txt"
+    )
+    chatgpt_api.set_target_answer_format(input_prompter.answer_format_str)
+
+    n_shots = eval_config["n_shots"]
+
+    n_test_samples = len(test_set)
+    print("n_test_samples: ", n_test_samples)
+    for test_idx, test_sample in enumerate(test_set):
+        samples = [train_set[random.randint(0, len(test_set))] for _ in range(n_shots)]
+        request_prompt = input_prompter.organize_test_prompt(
+            task_name=None, few_shot_samples=samples, test_sample=test_sample
+        )
+        do_model_request(chatgpt_api, request_prompt)
+
+
+datasets_eval = {"GSM8K": eval_gsm8k, "MMLU": eval_mmlu}
 
 
 def _main():
@@ -44,46 +120,16 @@ def _main():
     # define the environment used for learning
     devices, env_config = define_env(env_config=env_config)
 
-    #################### Prepare dataset ####################
-    # build data module
-    # for train
-    mmlu_data = mmlu_datasource()
-    train_set = mmlu_data.get_train_set()
-    test_set = mmlu_data.get_test_set()
-
+    #################### Prepare model ####################
     model_config = Config.items_to_dict(model_config._asdict())
     chatgpt_api = chatgpts.ChatGPTAPIRequest(model_config, env_config)
     chatgpt_api.get_authorization(
         organization=os.getenv("OPENAI_ORGAN_KEY"), api_key=os.getenv("OPENAI_API_KEY")
     )
 
-    task_name = "abstract algebra"
-    task_samples = train_set[task_name, -1]
-    test_sample = test_set[task_name, 0]
-
-    input_prompt = mmlu_prompt.MMLUStandardPrompt()
-    # input_prompt = mmlu_prompt.MMLUCoTPrompt(
-    #     cot_filepath="examples/LMs/ChainOfThought/mmlu-cot-claude.json"
-    # )
-    request_prompt = input_prompt.organize_test_prompt(
-        task_name, task_samples, test_sample
-    )
-    print(request_prompt)
-
-    chatgpt_api.set_target_answer_format(input_prompt.answer_format_str)
-    ipt_msg = chatgpt_api.create_messages(
-        textual_user_prompt=request_prompt,
-        textual_sys_prompt="Follow the given examples and answer the question.",
-    )
-    model_responses = chatgpt_api.perform_request(
-        messages=ipt_msg, per_request_responses=3
-    )
-    print("model_responses: ", model_responses)
-
-    extract_answer = chatgpt_api.extract_answer(model_responses)
-    print(extract_answer)
-    extract_target_answer = chatgpt_api.extract_target_answer_response(extract_answer)
-    print(extract_target_answer)
+    #################### Do evaluation for the dataset ####################
+    eval_config = Config.items_to_dict(eval_config._asdict())
+    datasets_eval[data_config.data_name](chatgpt_api, eval_config)
 
 
 if __name__ == "__main__":
