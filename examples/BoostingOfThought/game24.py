2d2d7dc122e5c8477cbc6ffb38c4a56a9c3bb6c6
Sijia Chen <CSJDeveloper@users.noreply.github.com>
2023-09-24 22:52:38 -0400

    Update game24.py

--- examples/BoostingOfThought/game24.py
+++ examples/BoostingOfThought/game24.py
@@ -39,13 +39,17 @@ def _main():
     if "model_type" in model_config and "llamav2" == model_config["model_type"]:
         request_model = llamav2.LLaMAV2Request(model_config, env_config)
 
-    thought = (
-        "This is a mathematical reasoning challenge, where the goal is to use four given numbers and basic arithmetic operations, including Addition, subtraction, multiplication, and division, to obtain 24. The given four numbers are: 2, 4, 5, 5.",
+    root_node = residual_tree_of_thoughts.ThoughtNode(
+        name="1",
+        thought="This is a mathematical reasoning challenge, where the goal is to use four given numbers and basic arithmetic operations, including Addition, subtraction, multiplication, and division, to obtain 24. The given four numbers are: 2, 4, 5, 5.",
+        thought_score=None,
     )
 
     gen_model = bot_model.ThoughtModel(request_model=request_model)
 
-    answers = gen_model.generate_thoughts(thoughts_chain=[()], num_thoughts=2)
+    answers = gen_model.generate_thoughts(
+        thoughts_node_chain=[root_node], num_thoughts=2
+    )
     print(answers)
 
 
