482c356454fbbdf069877e6dd902f4f2cb967a44
Sijia Chen <CSJDeveloper@users.noreply.github.com>
2023-11-14 21:32:29 -0500

    Removed the slurm running files

--- examples/run_scripts/cot_mmlu_llamav2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH --time=1:00:00
-#SBATCH --cpus-per-task=36
-#SBATCH --gres=gpu:1
-#SBATCH --mem=64G
-#SBATCH --output=cot_mmlu_llamav2.out
-
-torchrun --nproc_per_node 1 examples/ChainOfThought/ChainOfThought.py -c configs/MMLU/llamav2.yml -b ICLR
