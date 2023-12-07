b6729b2c09300765ee965b09d1d428b052938b91
Sijia Chen <CSJDeveloper@users.noreply.github.com>
2023-12-06 19:26:17 -0500

    Adjusted the prompting

--- llmpebase/model/prompting/base.py
+++ llmpebase/model/prompting/base.py
@@ -1,9 +1,9 @@
 """
 Basic implementation of prompting class.
 """
-
+import json
 import random
-from typing import List
+from typing import List, Union
 
 
 class BasePrompting:
@@ -12,28 +12,48 @@ class BasePrompting:
     other customized prompts.
     """
 
+    # For all the following variables, the punctuation should be included.
+    # Thus, no punctuation is needed to be added during organizing prompts.
     solution_flag: str = "The final solution is"
 
-    template_prompt_head: str = "Following examples are question-answer pairs about {}"
-    template_prompt_tail: str = "With above examples, please answer the given question"
+    question_prompt_head: str = "Question:"
+    question_prompt_tail: str = ""
 
-    notice: str = "Place the final solution after the sentence {} for readability"
+    answer_prompt_head: str = "Answer:"
+
+    template_prompt_head: str = (
+        "Following examples are question-answer pairs about {}.\n\n"
+    )
+    template_prompt_tail: str = (
+        "With above examples, please answer the given question.\n\n"
+    )
+
+    notice: str = "Place the final solution after the sentence '{}' at the end of the answer for readability."
 
     def __init__(self, model_config: dict = None):
         """ """
         self.model_config = model_config
 
     def organize_question_prompt(self, sample: dict):
-        """Organizing the question prompt."""
-        raise NotImplementedError(
-            "'organize_question_prompt' has not been implemented yet."
+        """Organize the question prompt."""
+        question = sample["question"]
+        prompt = (
+            f"""{self.question_prompt_head} {question}. {self.question_prompt_tail}\n"""
         )
+        return prompt
 
     def organize_answer_prompt(self, sample, is_answer_included=True):
-        """Organizing the answer prompt."""
-        raise NotImplementedError(
-            "'organize_answer_prompt' has not been implemented yet."
-        )
+        """Organize the answer prompt."""
+
+        if is_answer_included:
+            answer = sample["answer"]
+            groundtruth = sample["groundtruth"]
+            answer = "" if not is_answer_included else answer
+            groundtruth = "" if not is_answer_included else groundtruth
+
+            return f"""{self.answer_prompt_head} {answer}. {self.solution_flag} {groundtruth}."""
+
+        return f"""{self.answer_prompt_head}"""
 
     def organize_qa_prompt(self, sample: dict, is_answer_included=True):
         """Formatting the qa sample to a format structure."""
@@ -47,24 +67,27 @@ class BasePrompting:
 
     def organize_template_prompt(
         self,
-        samples: List[dict],
+        samples: Union[str, List[dict]] = None,
         problem_name: str = None,
     ):
         """organizing the prompt including the few-shot ."""
         problem_name = "" if problem_name is None else problem_name
-        fewshot = ""
-        if samples is not None:
+        fewshot = "" if samples is None else samples
+        if fewshot is not None and isinstance(fewshot, list):
             fewshot = "\n\n".join(
                 [self.organize_qa_prompt(sample) for sample in samples]
             )
 
         head = self.template_prompt_head.format(problem_name)
-        prompt = f"""{head}.\n\n{fewshot}\n\n{self.template_prompt_tail}."""
+        prompt = f"""{head}{fewshot}\n\n{self.template_prompt_tail}"""
 
         return prompt
 
     def create_test_prompt(
-        self, problem_name: str, test_sample: dict, template_samples: List[dict]
+        self,
+        problem_name: str,
+        test_sample: dict,
+        template_samples: Union[str, List[dict]],
     ):
         """Organizing the prompt for test."""
         fewshot_prompt = self.organize_template_prompt(template_samples, problem_name)
@@ -72,7 +95,7 @@ class BasePrompting:
 
         # Assign the solution flag to the notice
         notice = self.notice.format(self.solution_flag)
-        prompt = f"""{fewshot_prompt} \n\n Notice: {notice}\n\n{test_qa_prompt}"""
+        prompt = f"""{fewshot_prompt}Notice: {notice}\n\n{test_qa_prompt}"""
         return prompt
 
     def create_prompt_sample(self, sample, dataset, config):
@@ -84,8 +107,71 @@ class BasePrompting:
 
         return (
             self.create_test_prompt(
-                problem_name=sample.auxiliary["sample_problem"],
+                problem_name=sample["auxiliary"]["sample_problem"],
                 template_samples=samples,
+                test_sample=sample,
+            ),
+            sample["groundtruth"],
+        )
+
+
+class BaseCoTPrompting(BasePrompting):
+    """A base CoT prompting to load prompt from a file."""
+
+    answer_prompt_head: str = "Answer: Let's think step by step. "
+
+    def __init__(self, model_config: dict, cot_filepath: str = None) -> None:
+        super().__init__(model_config)
+
+        cot_filepath = (
+            cot_filepath if cot_filepath is not None else model_config["cot_filepath"]
+        )
+        self.cot_prompt = None
+        self.load_cot(cot_filepath)
+
+    def load_cot(self, cot_filepath: str):
+        """Load the cot examples from the file."""
+        with open(cot_filepath, "r", encoding="utf-8") as file:
+            self.cot_prompt = json.load(file)
+
+    def get_cot_prompt(self, problem_name: str, **kwargs):
+        """Load the cot prompt."""
+        problem_name = problem_name.replace(" ", "_").lower()
+        return self.cot_prompt[problem_name]
+
+    def create_prompt_sample(self, sample, dataset, config):
+        """Create one prompt sample."""
+        problem_name = sample["auxiliary"]["sample_problem"]
+        cot_samples = self.get_cot_prompt(problem_name)
+        return (
+            self.create_test_prompt(
+                problem_name=problem_name,
+                template_samples=cot_samples,
+                test_sample=sample,
+            ),
+            sample["groundtruth"],
+        )
+
+
+class BaseZeroShotPrompting(BasePrompting):
+    """A base zero-shot prompting"""
+
+    answer_prompt_head: str = "Answer: Let's think step by step."
+
+    template_prompt_head: str = "Answer the question about the problem {}"
+    template_prompt_tail: str = ""
+
+    def organize_answer_prompt(self, sample, is_answer_included=True):
+        """Organize the answer prompt."""
+        return f"""{self.answer_prompt_head}\n"""
+
+    def create_prompt_sample(self, sample, dataset, config):
+        """Create one prompt sample."""
+
+        return (
+            self.create_test_prompt(
+                problem_name=sample["auxiliary"]["sample_problem"],
+                template_samples=None,
                 test_sample=sample,
             ),
             sample["groundtruth"],
--- llmpebase/model/prompting/gsm8k.py
+++ llmpebase/model/prompting/gsm8k.py
@@ -1,7 +1,6 @@
 """
 The implementation of different prompts.
 """
-from typing import List
 
 from llmpebase.model.prompting import base
 
@@ -9,102 +8,23 @@ from llmpebase.model.prompting import base
 class GSM8KStandardPrompting(base.BasePrompting):
     """The standard prompt of GSM8K."""
 
-    def organize_question_prompt(self, sample: dict):
-        """Organizing the question prompt."""
-        ques = sample["question"]
-        prompt = f"""Question: {ques} \n"""
-        return prompt
 
-    def organize_answer_prompt(self, sample, is_answer_included=True):
-        """Organizing the answer prompt."""
-        answer = sample["answer"]
-        groundtruth = sample["groundtruth"]
-        if is_answer_included:
-            answer = "" if not is_answer_included else answer
-            groundtruth = "" if not is_answer_included else groundtruth
-
-            return f"""Answer: {answer}. {self.solution_flag} {groundtruth}"""
-        return """Answer: """
-
-
-class GSM8KCoTPrompting(GSM8KStandardPrompting):
+class GSM8KCoTPrompting(base.BaseCoTPrompting):
     """The CoT prompt of GSM8K."""
 
     # This should be the same as the answer format in the cot_filepath
     # Current CoT ones use "The answer is".
     solution_flag: str = "The answer is "
 
-    def __init__(self, model_config: dict, cot_filepath: str = None) -> None:
-        super().__init__()
-        cot_filepath = (
-            cot_filepath if cot_filepath is not None else model_config["cot_filepath"]
-        )
-        with open(cot_filepath, "r", encoding="utf-8") as txt_file:
-            self.cot_prompt = txt_file.read()
+    def load_cot(self, cot_filepath: str):
+        """Load the cot examples from the file."""
+        with open(cot_filepath, "r", encoding="utf-8") as file:
+            self.cot_prompt = file.read()
 
-    def organize_answer_prompt(self, sample, is_answer_included=True):
-        """Organizing the answer prompt."""
-        answer = sample["answer"]
-        if is_answer_included:
-            return f"""Answer: Let's think step by step. {answer}"""
-
-        return """Answer: Let's think step by step. """
-
-    def organize_template_prompt(
-        self,
-        samples: List[dict],
-        problem_name: str = None,
-    ):
-        """organizing the prompt including the few-shot ."""
-        head = self.template_prompt_head.format(problem_name)
-        prompt = f"""{head}.\n\n {self.cot_prompt}\n\n{self.template_prompt_tail}."""
-        return prompt
-
-    def create_prompt_sample(self, sample, dataset, config):
-        """Evaluating the GSM8K dataset."""
-
-        problem_name = sample.auxiliary["sample_problem"]
-        return (
-            self.create_test_prompt(
-                problem_name=problem_name,
-                test_sample=sample,
-                template_samples=None,
-            ),
-            sample["groundtruth"],
-        )
+    def get_cot_prompt(self, problem_name: str, **kwargs):
+        """Load the cot prompt."""
+        return self.cot_prompt
 
 
-class GSM8KZeroShotCoTPrompting(GSM8KStandardPrompting):
+class GSM8KZeroShotCoTPrompting(base.BaseZeroShotPrompting):
     """The zeroshot CoT prompt of GSM8K."""
-
-    def organize_answer_prompt(self, sample, is_answer_included=True):
-        """Organize the answer prompt."""
-        return """Answer: Let's think step by step.\n"""
-
-    def organize_template_prompt(
-        self,
-        samples: List[dict],
-        problem_name: str = None,
-    ):
-        """No template prompt for zeroshot."""
-        return ""
-
-    def create_test_prompt(
-        self, problem_name: str, test_sample: dict, template_samples: List[dict]
-    ):
-        """Organizing the prompt for test."""
-        test_qa_prompt = self.organize_qa_prompt(test_sample, is_answer_included=False)
-        return test_qa_prompt
-
-    def create_prompt_sample(self, sample, dataset, config):
-        """Evaluating the GSM8K dataset."""
-
-        problem_name = sample.auxiliary["sample_problem"]
-        return (
-            self.create_test_prompt(
-                problem_name=problem_name,
-                test_sample=sample,
-                template_samples=None,
-            ),
-            sample["groundtruth"],
-        )
