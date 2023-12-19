7028c283cb59fae778a597ed39dd5ffc6b506c49
Sijia Chen <CSJDeveloper@users.noreply.github.com>
2023-12-19 17:18:09 -0500

    Added the thought chain extraction

--- examples/ChainReasoning/reasoner.py
+++ examples/ChainReasoning/reasoner.py
@@ -7,6 +7,7 @@ from torch import nn
 from llmpebase.model.thought_structure import chains
 from llmpebase.model.prompting.base import BasicPromptSample
 from llmpebase.model.thought_structure.visualization import BasicStructureVisualizer
+from llmpebase.model.thought_structure.chain_extractors import ChainExtractor
 
 
 class ChainReasoner:
@@ -33,6 +34,8 @@ class ChainReasoner:
             model_config, logging_config=logging_config
         )
 
+        self.solution_extractor = ChainExtractor()
+
     def forward(self, prompt_sample: BasicPromptSample):
         """Forward the reasoning in the chain structure."""
         # Make changes to the prompt sample
@@ -43,3 +46,13 @@ class ChainReasoner:
         self.structure.build_structure(
             thought_model=self.thought_model, visualizer=self.visualizer
         )
+
+        # Get the chain and save it
+        solution_chain = self.solution_extractor.extract_thought_chain(self.structure)
+        self.structure.save_thought_path(solution_chain, path_name="solution_chain")
+        # Convert the chain into a string
+        solution_str = self.thought_model.prompter.organize_chain_prompt(
+            chain_nodes=solution_chain
+        )
+
+        return solution_str
