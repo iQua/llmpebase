25266eeb59f39183123f4c0088a578e19e98a1e5
Sijia Chen <CSJDeveloper@users.noreply.github.com>
2024-01-12 14:15:55 -0500

    Improved the visualization

--- llmpebase/model/thought_structure/visualization.py
+++ llmpebase/model/thought_structure/visualization.py
@@ -15,20 +15,20 @@ node_config = {
     "Root": {
         "node_color": "#228B22",
         "node_shape": "o",
-        "node_size": 1100,
-        "alpha": 0.9,
+        "node_size": 900,
+        "alpha": 0.8,
     },
     "Intermediate": {
         "node_color": "#6495ED",
         "node_shape": "s",
-        "node_size": 1200,
+        "node_size": 900,
         "alpha": 0.9,
     },
-    "Stop": {
+    "Sink": {
         "node_color": "#F5DEB3",
         "node_shape": "8",
-        "node_size": 1100,
-        "alpha": 0.9,
+        "node_size": 900,
+        "alpha": 0.8,
     },
 }
 
@@ -37,19 +37,19 @@ edge_config = {
         "edge_color": "black",
         "width": 1.111111110,
         "style": "solid",
-        "arrowsize": 20,
+        "arrowsize": 10,
     },
     "Intermediate": {
         "edge_color": "gray",
         "width": 1.0,
         "style": "solid",
-        "arrowsize": 20,
+        "arrowsize": 10,
     },
-    "Stop": {
+    "Sink": {
         "edge_color": "green",
         "width": 1.0,
         "style": "solid",
-        "arrowsize": 20,
+        "arrowsize": 10,
     },
 }
 
@@ -70,6 +70,63 @@ class BasicStructureVisualizer:
         self.visualization_path = logging_config["visualization_path"]
         self.visualization_foldername = visualization_foldername
 
+    def draw_node(
+        self,
+        ax: plt.axes,
+        graph: nx.Graph,
+        pos: dict,
+        node: BasicNode,
+    ):
+        """Draw a node of the graph."""
+        node_id = node.identity
+        pos_type = node.position
+        nx.draw_networkx_nodes(
+            graph,
+            pos,
+            nodelist=[node_id],
+            ax=ax,
+            **node_config[pos_type],
+        )
+        return ax
+
+    def draw_node_edges(
+        self,
+        ax: plt.axes,
+        graph: nx.Graph,
+        pos: dict,
+        node: BasicNode,
+    ):
+        """Draw the edges of one node of the graph."""
+
+        pos_type = node.position
+        node_id = node.identity
+
+        # Draw edges of the node
+        edges = [(node_id, n) for n in graph.neighbors(node_id)]
+        # The 'node_shape' is commented out as it will make
+        # the arrow not stick to the node
+        nx.draw_networkx_edges(
+            graph,
+            pos,
+            edgelist=edges,
+            ax=ax,
+            node_size=node_config[pos_type]["node_size"],
+            # node_shape=node_config[pos_type]["node_shape"],
+            **edge_config[pos_type],
+        )
+
+        return ax
+
+    def create_draw_labels(self, graph: nx.Graph, node_pool: List[BasicNode]):
+        """Create the labels of the node in the graph draw."""
+        labels = {
+            node_pool[
+                node_id
+            ].identity: f"N-{node_pool[node_id].identity}\n S-{node_pool[node_id].step_idx}"
+            for node_id in graph.nodes
+        }
+        return labels
+
     def draw_graph(
         self,
         ax: plt.axes,
@@ -83,43 +140,30 @@ class BasicStructureVisualizer:
 
         # Get the positions
         pos = graphviz_layout(graph, prog="dot")
-        labels = {
-            node_pool[
-                node_id
-            ].identity: f"Node {node_pool[node_id].identity}\n Step {node_pool[node_id].step_idx}"
-            for node_id in graph.nodes
-        }
+        # Create the labels to be plotted
+        labels = self.create_draw_labels(graph=graph, node_pool=node_pool)
         for node_id in graph.nodes:
             node = node_pool[node_id]
-            pos_type = node.position
+
             node_id = node.identity
-            # Draw all nodes
-            nx.draw_networkx_nodes(
-                graph,
-                pos,
-                nodelist=[node_id],
-                ax=ax,
-                **node_config[pos_type],
-            )
-            # Draw edges of Root
-            edges = [(node_id, n) for n in graph.neighbors(node_id)]
-            nx.draw_networkx_edges(
-                graph,
-                pos,
-                edgelist=edges,
-                ax=ax,
-                **edge_config[pos_type],
-            )
+            # Draw the node
+            ax = self.draw_node(ax=ax, graph=graph, pos=pos, node=node)
+            # Draw edges of the node
+            ax = self.draw_node_edges(ax=ax, graph=graph, pos=pos, node=node)
+
             # A root node is a node with no predecessors
             if graph.in_degree(node_id) == 0:
-                labels[node_id] = "Start"
+                labels[node_id] = "Q"
 
         nx.draw_networkx_labels(graph, pos, labels, ax=ax, **labels_config)
 
         return ax
 
     def visualize(
-        self, graph: nx.Graph, node_pool: List[BasicNode], save_name: str = None
+        self,
+        graph: nx.Graph,
+        node_pool: List[BasicNode],
+        save_name: str = None,
     ):
         """Plot the thought structure."""
 
