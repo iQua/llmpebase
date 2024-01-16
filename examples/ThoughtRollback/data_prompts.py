c0b9a502d88a7d2e9973f6bdf4b7391578f8a0c7
Sijia Chen <CSJDeveloper@users.noreply.github.com>
2024-01-15 23:34:50 -0500

    Revised the error analysis of TR

--- examples/ThoughtRollback/data_prompts.py
+++ examples/ThoughtRollback/data_prompts.py
@@ -8,7 +8,7 @@ from llmpebase.model.prompting.prompt_generic import (
 rollback_controller_prompt_format = BasicThoughtPromptFormat(
     head="{}Toward addressing the given question, below is a reasoning process containing {} steps and the corresponding analysis: \n",
     content="\n{}\n{}\n{}\n",
-    target="""Please summarize the analysis within {}, and thus from the given {} steps, extract the indexes of those unnecessary, wrong, illogical, unreasonable, or vague reasoning steps. Only output the indexes after '{}'. """,
+    target="""Please summarize the analysis within {}, and thus within the given {} steps, extract the indexes of those unnecessary, wrong, illogical, unreasonable, or vague reasoning steps. Only output the indexes after '{}'. """,
     notice="Output None when no steps are given.\n",
     tail="",
     prompt="",
@@ -18,7 +18,7 @@ rollback_controller_prompt_format = BasicThoughtPromptFormat(
 mathematical_reasoning_analysis_prompt_format = BasicThoughtPromptFormat(
     head="{}Toward addressing the given question, below is a reasoning process containing {} steps: \n",
     content="\n{}\n\n",
-    target="""Double-check the reasoning process within {}, please analyze its overall and each step's correctness by checking 1) whether they are mathematical logic and rationality; 2) whether logic among them is correct. Please also report an error when any step does not contain a clear mathematical expression. """,
+    target="""Double-check the reasoning process within {}, please analyze its overall and each step's correctness by checking whether they are mathematical logic and rationality. Please report an error when any step does not contain a clear mathematical expression. """,
     notice="Output empty string when no steps are given.\n",
     tail="",
     prompt="",
@@ -27,15 +27,13 @@ mathematical_reasoning_analysis_prompt_format = BasicThoughtPromptFormat(
 mathematical_solution_analysis_prompt_format = BasicThoughtPromptFormat(
     head="{}Toward addressing the given question, below is a reasoning process containing {} steps: \n",
     content="\n{}\n\n",
-    target="""Double-check the reasoning process within {}, please analyze its overall and each step's correctness by checking whether they are mathematical logic, rationality; Please check 1) whether the final solution is presented as the numerical value; 2) whether the solution can be used to answer the given question '{}'. Please specifically report an error when the variables in the final solution are not substituted with the given value. """,
+    target="""Double-check the reasoning process within {}, please analyze its overall and each step's correctness by checking whether they are mathematical logic, rationality; Please check whether the final solution can be used to answer the given question '{}'. Please specifically report an error when the variables in the final solution are not substituted with the given value. """,
     notice="Output empty string when no steps are given.\n",
     tail="",
     prompt="",
 )
 
 rollback_prompt_formats = {
-    "GSM8K": {
-        "Intermediate": mathematical_reasoning_analysis_prompt_format,
-        "Sink": mathematical_solution_analysis_prompt_format,
-    }
+    "Intermediate": mathematical_reasoning_analysis_prompt_format,
+    "Sink": mathematical_solution_analysis_prompt_format,
 }
